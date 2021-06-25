84a28b5a9574fb159cbcc37b435f853ba2f9710f
Morsalin Kausar <morsalinkausar043@gmail.com>
2021-06-25 23:08:19 +0600

    massage nai but done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8000 ;
 
+// eituku dile css er path thik thakbe pore index.hbs file pore niye nibo
+
 const staticpath = path.join(__dirname , "../public/src");
 app.use(express.static(staticpath));
 
+// views er name change kore templete leka hoiche tai emn korsi
+
+const templetepath = path.join(__dirname , "../templete");
 app.set('view engine' , 'hbs');
+app.set('views' , templetepath)
 
 app.get('' , (req, res) =>{
     res.status(201).render('index',{name:"Morsalin"});
